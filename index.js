18304af45cf1e8b0cd93668e5e8156d2ffdf8861
hrfmmymt <hrfmmymt@gmail.com>
2021-08-06 23:23:39 +0900

    feat: get post

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fastify = require("fastify")({
   logger: true,
 });
+const marked = require("marked");
+const fs = require("fs");
+const path = require("path");
+
+marked.setOptions({
+  gfm: true,
+});
 
 fastify.register(require("point-of-view"), {
   engine: {
@@ -10,6 +17,20 @@ fastify.register(require("point-of-view"), {
 
 fastify.get("/", (req, reply) => {
   reply.view("/templates/index.njk", { title: "title" });
+});
+
+fastify.get("/:post", (req, reply) => {
+  console.time("Render " + req.params.post);
+  console.log("Requested: " + req.params.post + " at " + Date.now());
+
+  let file = __dirname + "/post/" + req.params.post + ".md";
+
+  fs.readFile(file, "utf-8", (err, contents) => {
+    if (err) throw err;
+    reply.send(marked(contents));
+  });
+
+  console.timeEnd("Render " + req.params.post);
 });
 
 fastify.listen(3000, (err) => {
