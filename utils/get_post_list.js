22022a7fce812fe68c9ce420c2eadfce66fcc688
hrfmmymt <hrfmmymt@gmail.com>
2021-08-10 18:56:28 +0900

    separate

--- utils/get_post_list.js
+++ utils/get_post_list.js
@@ -1,36 +1,9 @@
 const fs = require('mz/fs');
 const path = require('path');
-const marked = require('marked');
+
+const getPostInfo = require('./get_post_info');
 
 const postDir = path.join(__dirname, '../post/');
-
-const getPostInfo = (fileName, withHtml) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(postDir + fileName, 'utf-8', (err, md) => {
-      if (err) return reject(err);
-
-      const postTitle = md.match(/^#\s.+\n/)[0].match(/[^#\n]+/);
-      const postDescription = /\n\*desc>\s((?:(?!\*\n)[^\s　])+)/g.exec(
-        md.match(/\n\*desc>\s(.)+\n/)[0]
-      );
-      const postDate = /\*date\:((?:(?!\*)[^\s　])+)/g.exec(md);
-
-      marked.setOptions({
-        gfm: true,
-      });
-
-      resolve({
-        title: postTitle[0].trim(),
-        description: postDescription[1],
-        date: postDate[1],
-        url: fileName.replace(/.md/g, ''),
-        html: withHtml ? marked(md) : null,
-      });
-    });
-  });
-};
-
-module.exports = getPostInfo;
 
 async function sortPostsList() {
   const dist = path.join(__dirname, '../');
