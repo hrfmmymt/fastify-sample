58ccff6e874b74eef2b94f3524a556bfa8b687db
hrfmmymt <hrfmmymt@gmail.com>
2021-08-11 09:59:51 +0900

    convert `get_post_info.js` to TS

--- utils/get_post_info.js
+++ utils/get_post_info.js
@@ -1,33 +1,38 @@
-const fs = require('mz/fs');
-const path = require('path');
-const marked = require('marked');
-
-const postDir = path.join(__dirname, '../post/');
-
-const getPostInfo = (fileName, withHtml) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(postDir + fileName, 'utf-8', (err, md) => {
-      if (err) return reject(err);
-
-      const postTitle = md.match(/^#\s.+\n/)[0].match(/[^#\n]+/);
-      const postDescription = /\n\*desc>\s((?:(?!\*\n)[^\s　])+)/g.exec(
-        md.match(/\n\*desc>\s(.)+\n/)[0]
-      );
-      const postDate = /\*date\:((?:(?!\*)[^\s　])+)/g.exec(md);
-
-      marked.setOptions({
-        gfm: true,
-      });
-
-      resolve({
-        title: postTitle[0].trim(),
-        description: postDescription[1],
-        date: postDate[1],
-        url: fileName.replace(/.md/g, ''),
-        html: withHtml ? marked(md) : null,
-      });
-    });
-  });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
-module.exports = getPostInfo;
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const marked_1 = __importDefault(require("marked"));
+const postDir = path_1.default.join(__dirname, '../post/');
+function getPostInfo(fileName, withHtml) {
+    return new Promise((resolve, reject) => {
+        fs_1.default.readFile(postDir + fileName, 'utf-8', (err, md) => {
+            if (err)
+                return reject(err);
+            const h1 = md.match(/^#\s.+\n/);
+            const postTitle = h1 ? h1[0].match(/[^#\n]+/) : null;
+            const title = postTitle ? postTitle[0].trim() : '';
+            const desc = md.match(/\n\*desc>\s(.)+\n/);
+            const postDescription = desc
+                ? /\n\*desc>\s((?:(?!\*\n)[^\s　])+)/g.exec(desc[0])
+                : null;
+            const description = postDescription ? postDescription[1] : '';
+            const postDate = /\*date\:((?:(?!\*)[^\s　])+)/g.exec(md);
+            marked_1.default.setOptions({
+                gfm: true,
+            });
+            resolve({
+                title,
+                description,
+                date: postDate ? postDate[1] : '',
+                url: fileName.replace(/.md/g, ''),
+                html: withHtml ? marked_1.default(md) : null,
+            });
+        });
+    });
+}
+exports.default = getPostInfo;
+// module.exports = getPostInfo;
---
+++ utils/get_post_info.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import marked from 'marked';
+
+const postDir = path.join(__dirname, '../post/');
+
+export default function getPostInfo(
+  fileName: string,
+  withHtml?: string | boolean
+) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(postDir + fileName, 'utf-8', (err, md) => {
+      if (err) return reject(err);
+
+      const h1 = md.match(/^#\s.+\n/);
+      const postTitle = h1 ? h1[0].match(/[^#\n]+/) : null;
+      const title = postTitle ? postTitle[0].trim() : '';
+
+      const desc = md.match(/\n\*desc>\s(.)+\n/);
+      const postDescription = desc
+        ? /\n\*desc>\s((?:(?!\*\n)[^\s　])+)/g.exec(desc[0])
+        : null;
+      const description = postDescription ? postDescription[1] : '';
+
+      const postDate = /\*date\:((?:(?!\*)[^\s　])+)/g.exec(md);
+
+      marked.setOptions({
+        gfm: true,
+      });
+
+      resolve({
+        title,
+        description,
+        date: postDate ? postDate[1] : '',
+        url: fileName.replace(/.md/g, ''),
+        html: withHtml ? marked(md) : null,
+      });
+    });
+  });
+}
+
+// module.exports = getPostInfo;
