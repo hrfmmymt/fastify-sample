21a0a4eb18c64c53f0bb11d133289ca7b34860a2
hrfmmymt <hrfmmymt@gmail.com>
2021-08-21 21:40:03 +0900

    add test_get_post_info

---
+++ test/test_get_post_info.js
@@ -0,0 +1,32 @@
+'use strict';
+const tap = require('tap');
+
+const { getPostInfo } = require('../utils/get_post_info');
+
+const expect = {
+  title: '0',
+  description: 'enable',
+  date: '1999-09-11',
+  url: '0',
+  html: null,
+};
+
+tap.test('return data if `withHtml` is true', (t) => {
+  return getPostInfo({ fileName: '0.md', withHtml: true }).then((data) => {
+    t.equal(data.title, expect.title);
+    t.equal(data.description, expect.description);
+    t.equal(data.date, expect.date);
+    t.equal(data.url, expect.url);
+    t.not(data.html, null);
+  });
+});
+
+tap.test('return html null if `withHtml` is false', async (t) => {
+  return getPostInfo({ fileName: '0.md', withHtml: false }).then((data) => {
+    t.equal(data.title, expect.title);
+    t.equal(data.description, expect.description);
+    t.equal(data.date, expect.date);
+    t.equal(data.url, expect.url);
+    t.equal(data.html, expect.html);
+  });
+});
