217b945880a0fb8d6ab6ab4c88376889e443162e
hrfmmymt <hrfmmymt@gmail.com>
2021-08-23 10:01:53 +0900

    add test

--- test/test_app.ts
+++ test/test_app.ts
@@ -41,3 +41,45 @@ tap.test('GET `/:post` post', (t) => {
     }
   );
 });
+
+tap.test('GET `/api` api', (t) => {
+  t.plan(3);
+
+  const fastify = build();
+
+  t.teardown(() => fastify.close());
+
+  fastify.inject(
+    {
+      method: 'GET',
+      url: '/api',
+    },
+    (err: Error, response: any) => {
+      t.error(err);
+      t.equal(response.statusCode, 200);
+      t.equal(
+        response.headers['content-type'],
+        'application/json; charset=utf-8'
+      );
+    }
+  );
+});
+
+tap.test('GET `/favicon.ico` favicon.ico', (t) => {
+  t.plan(2);
+
+  const fastify = build();
+
+  t.teardown(() => fastify.close());
+
+  fastify.inject(
+    {
+      method: 'GET',
+      url: '/favicon.ico',
+    },
+    (err: Error, response: any) => {
+      t.error(err);
+      t.equal(response.statusCode, 404);
+    }
+  );
+});
