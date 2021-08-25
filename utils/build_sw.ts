64c38a70bcfd7793abd65dc8e73c6d2643df79d1
hrfmmymt <hrfmmymt@gmail.com>
2021-08-25 10:08:57 +0900

    use ts (wip)

--- utils/build_sw.ts
+++ utils/build_sw.ts
@@ -1,14 +1,12 @@
-const workboxBuild = require('workbox-build');
+import * as workboxBuild from 'workbox-build';
 
 // NOTE: This should be run *AFTER* all your assets are built
 const buildSW = () => {
   // This will return a Promise
   return workboxBuild.generateSW({
-    globDirectory: './public',
-    globPatterns: [
-      '**/*.{js,css,png,jpg}',
-      'post/*.md',
-    ],
+    globDirectory: './',
+    globIgnores: ['./node_modules/**'],
+    globPatterns: ['./**/*.{css,png,jpg}'],
     ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
     swDest: 'public/sw.js',
   });
