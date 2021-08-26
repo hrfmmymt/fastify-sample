6ee3f008d3079d077dcf1992ee9baa4dd46c3849
hrfmmymt <hrfmmymt@gmail.com>
2021-08-27 00:22:03 +0900

    works sw.js

---
+++ sw.js
@@ -0,0 +1,85 @@
+const CACHENAME = '::iiyatsu_serviceworker';
+const VERSION = 'v0.0.1';
+const OFFLINEFALLBACKPAGE = 'offline.html';
+const URLS_TO_CACHE = [
+  './',
+  './0',
+  './1%E3%81%AE%E3%82%B3%E3%83%92%E3%82%9A%E3%83%BC',
+  './1',
+  './public/css/style.css',
+  './public/js/script.js',
+  './public/manifest.json',
+  './sw.js',
+];
+
+const preLoad = async () => {
+  console.log('Installing web app');
+  try {
+    const cache = await caches.open(VERSION + CACHENAME);
+    const cachedUrls = cache.addAll(URLS_TO_CACHE);
+    return cachedUrls;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(preLoad());
+  console.log('installed latest version');
+});
+
+const makeNetWorkRequest = (request) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const networkFetchResponse = await fetch(request);
+      if (networkFetchResponse.status !== 404) {
+        resolve(networkFetchResponse);
+      } else {
+        throw new Error('no resource found');
+      }
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
+
+const returnFromCache = async (request) => {
+  try {
+    const cache = await caches.open(VERSION + CACHENAME);
+    const cacheItemMatchingNetworkRequest = await cache.match(request);
+    if (
+      !cacheItemMatchingNetworkRequest ||
+      cacheItemMatchingNetworkRequest.status == 404
+    ) {
+      return cache.match('/');
+    } else {
+      return cacheItemMatchingNetworkRequest;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    makeNetWorkRequest(event.request).catch(() => {
+      return returnFromCache(event.request);
+    })
+  );
+});
+
+// This is an event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener('refreshOffline', function () {
+  const offlinePageRequest = new Request(OFFLINEFALLBACKPAGE);
+
+  return fetch(OFFLINEFALLBACKPAGE).then(function (response) {
+    return caches.open(VERSION + CACHENAME).then(function (cache) {
+      console.log(
+        '[PWA Builder] Offline page updated from refreshOffline event: ' +
+          response.url
+      );
+      return cache.put(offlinePageRequest, response);
+    });
+  });
+});
