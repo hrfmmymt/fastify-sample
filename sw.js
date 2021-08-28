8b5774832f4aa2319d8c6caef5e4b6f682f6564b
hrfmmymt <hrfmmymt@gmail.com>
2021-08-28 15:23:26 +0900

    modified

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHENAME = '::iiyatsu_serviceworker';
 const VERSION = 'v0.0.1';
-const OFFLINEFALLBACKPAGE = 'offline.html';
+const OFFLINEFALLBACKPAGE = './public/offline.html';
 const URLS_TO_CACHE = [
   './',
   './sw.js',
@@ -24,74 +24,47 @@ const URLS_TO_CACHE = [
   './public/manifest.json',
 ];
 
-const preLoad = async () => {
-  console.log('Installing web app');
-  try {
-    const cache = await caches.open(VERSION + CACHENAME);
-    const cachedUrls = cache.addAll(URLS_TO_CACHE);
-    return cachedUrls;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 self.addEventListener('install', (event) => {
+  console.log('The service worker is being installed.');
   self.skipWaiting();
-  event.waitUntil(preLoad());
-  console.log('installed latest version');
-});
-
-const makeNetWorkRequest = (request) =>
-  new Promise(async (resolve, reject) => {
-    try {
-      const networkFetchResponse = await fetch(request);
-      if (networkFetchResponse.status !== 404) {
-        resolve(networkFetchResponse);
-      } else {
-        throw new Error('no resource found');
-      }
-    } catch (error) {
-      console.error(error);
-      reject(error);
-    }
-  });
-
-const returnFromCache = async (request) => {
-  try {
-    const cache = await caches.open(VERSION + CACHENAME);
-    const cacheItemMatchingNetworkRequest = await cache.match(request);
-    if (
-      !cacheItemMatchingNetworkRequest ||
-      cacheItemMatchingNetworkRequest.status == 404
-    ) {
-      return cache.match('/');
-    } else {
-      return cacheItemMatchingNetworkRequest;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    makeNetWorkRequest(event.request).catch(() => {
-      return returnFromCache(event.request);
-    })
+  event.waitUntil(
+    caches.open(VERSION + CACHENAME).then((cache) => {
+      cache.addAll(URLS_TO_CACHE);
+    }, console.err)
   );
 });
 
-// This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener('refreshOffline', function () {
-  const offlinePageRequest = new Request(OFFLINEFALLBACKPAGE);
+const fromCache = (request) => {
+  return caches.open(VERSION + CACHENAME).then((cache) => {
+    return cache.match(request);
+  });
+};
 
-  return fetch(OFFLINEFALLBACKPAGE).then(function (response) {
-    return caches.open(VERSION + CACHENAME).then(function (cache) {
-      console.log(
-        '[PWA Builder] Offline page updated from refreshOffline event: ' +
-          response.url
-      );
-      return cache.put(offlinePageRequest, response);
+const update = (request) => {
+  return caches.open(VERSION + CACHENAME).then((cache) => {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(() => {
+        return response;
+      });
     });
   });
+};
+
+const refresh = (response) => {
+  return self.clients.matchAll().then((clients) => {
+    clients.forEach((client) => {
+      const message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag'),
+      };
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+};
+
+self.addEventListener('fetch', (event) => {
+  console.log('The service worker is serving the asset.');
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request).then(refresh));
 });
