522ca45c1af707b043fcc0347c3e6a3d709ca6e1
hrfmmymt <hrfmmymt@gmail.com>
2021-08-29 00:31:22 +0900

    set meta

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import * as path from 'path';
 import { getPostInfo } from '../utils/get_post_info';
 import { checkFileExistence } from '../utils/check_file_existence';
 
+const COMMON_TITLE = "iiyatsu - hrfmmymt's weblog";
+const PUBLIC_URL = 'https://iiyatsu.hrfmmymt.com/';
+
 const config = {
+  currentYear: new Date().getFullYear(),
   postDir: path.join(__dirname, '../post/'),
   postList: JSON.parse(
     fs.readFileSync(path.join(__dirname, '../post-list.json'), 'utf8')
@@ -16,11 +20,13 @@ const metadata = {
   author: 'hrfmmymt',
   copyright:
     'Copyright &copy; 2021 fastify-sample of hrfmmymt All Rights Reserved.',
+  description: "hrfmmymt's weblog",
   ogImage: 'public/img/icons/icon.png',
   favicon: 'public/img/icons/favicon.ico',
-  title: "hrfmmymt's fastify-sample",
-  twitterSite: '@hrfmmymt',
+  title: COMMON_TITLE,
+  twitterAccount: '@hrfmmymt',
   twitterCard: 'summary',
+  url: PUBLIC_URL,
 };
 
 function build(opts = {}) {
@@ -40,15 +46,23 @@ function build(opts = {}) {
   app.get('/', (_req, reply: any) => {
     reply.view('./templates/index.njk', {
       head: {
-        title: metadata.title,
-        url: '',
-        description: '',
+        author: metadata.author,
+        description: metadata.description,
         favicon: metadata.favicon,
         ogImage: metadata.ogImage,
-        twitterSite: metadata.twitterSite,
-        twitterCard: metadata.twitterSite,
+        ogType: 'website',
+        title: metadata.title,
+        twitterAccount: metadata.twitterAccount,
+        twitterImage: metadata.ogImage,
+        twitterSite: metadata.twitterAccount,
+        twitterCard: metadata.twitterAccount,
+        url: metadata.url,
+        year: config.currentYear,
       },
       postList: config.postList,
+      footer: {
+        year: config.currentYear,
+      },
     });
   });
 
@@ -67,15 +81,24 @@ function build(opts = {}) {
       reply.view('./templates/page/post.njk', {
         postList: null,
         head: {
-          title: postInfo.title,
-          url: postInfo.url,
+          author: metadata.author,
           description: postInfo.description,
+          favicon: metadata.favicon,
           ogImage: metadata.ogImage,
-          twitterSite: metadata.twitterSite,
-          twitterCard: metadata.twitterSite,
+          ogType: 'article',
+          title: `${postInfo.title} | ${metadata.title}`,
+          twitterAccount: metadata.twitterAccount,
+          twitterImage: metadata.ogImage,
+          twitterSite: metadata.twitterAccount,
+          twitterCard: metadata.twitterAccount,
+          url: `${metadata.url}${postInfo.url}`,
+          year: config.currentYear,
         },
         post: {
           contents: postInfo.html,
+        },
+        footer: {
+          year: config.currentYear,
         },
       });
     });
@@ -89,6 +112,10 @@ function build(opts = {}) {
     fs.readFile('./sw.js', 'utf-8', (err, fileBuffer) => {
       reply.type('text/javascript').send(err || fileBuffer);
     });
+  });
+
+  app.get('/favicon.ico', (_req, reply) => {
+    reply.code(404).send();
   });
 
   app.get('/robots.txt', (_req, reply) => {
