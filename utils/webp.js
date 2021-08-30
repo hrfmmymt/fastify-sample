f7574c562797437b36259dcc489b71c792c8d94b
hrfmmymt <hrfmmymt@gmail.com>
2021-08-30 11:51:48 +0900

    works imagemin-webp

--- utils/webp.js
+++ utils/webp.js
@@ -1,5 +1,10 @@
-import imagemin from 'imagemin';
-import imageminWebp from 'imagemin-webp';
+/* now `imagemin` version is 7.0.1
+ * ESM compatible is not yet supported by update the version to 8 series
+ */
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+// import imagemin from 'imagemin';
+// import imageminWebp from 'imagemin-webp'
 
 (async () => {
   await imagemin(['./public/img/post/*.{jpg,png}'], {
